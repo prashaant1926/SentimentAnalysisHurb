058a09e6a60a0583a91194692312f9a78c0a78e2
prashaant1926 <58019366+prashaant1926@users.noreply.github.com>
2021-11-10 12:46:11 -0300

    Add files via upload

---
+++ PortuguesePolarityCalculator.R
@@ -0,0 +1,24 @@
+ptPolarityCalculator <- function( sentencePt ){
+  score <- 0
+  ctr <- 0
+  sentencePt <-  gsub("[[:punct:]]", "", sentencePt) #removing all punctuation 
+  sentencePt <- gsub("[\r\n]", " ", sentencePt)
+  sentencePt <- unlist(strsplit(sentencePt, " "))
+  sentencePt <- as.vector(sentencePt)
+  for (st in sentencePt){
+    wordPolarity <- as.data.frame(get_word_sentiment(st))
+    ctr <- ctr + 1
+    if(is.null(wordPolarity$oplexicon_v3.0.polarity)){
+      score <- 0 + score
+    }
+    else{
+      score <- wordPolarity$oplexicon_v3.0.polarity+ score
+    }
+  }
+  totalScore<- score/ctr
+  score <- 0
+  ctr <- 0
+  
+  print(totalScore)
+  return(totalScore)
+}
